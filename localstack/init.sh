368f3dded652807107090c72ed4e29075005d9de
akianzadeh <ali.kianzadeh@sony.com>
2023-08-03 09:15:08 -0700

    working on secret manager

---
+++ localstack/init.sh
@@ -0,0 +1,8 @@
+aws secretsmanager delete-secret \
+    --secret-id /secrets/HeroesSecrets \
+    --force-delete-without-recovery \
+    --endpoint-url http://localhost:4566//
+aws secretsmanager create-secret \
+    --name /secrets/HeroesSecrets \
+    --secret-string file://secrets.json \
+    --endpoint-url http://localhost:4566//
